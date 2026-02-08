a7eb25461fd82e1e3b2bf3b83f8166b9a1e948c2
Grayson S <grayson@infocusdata.com>
2026-02-07 19:44:38 -0600

    install tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0', // Listen on all interfaces
   },
+  plugins: [
+    tailwindcss(),
+  ],
 });
 
